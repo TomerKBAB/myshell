95e0ec631cfffed397c8fca40622de0d18f23cbd
Tomer R <trrrt1999@gmail.com>
2025-04-30 11:20:31 +0000

    Part 2: Implementing a Pipe in the Shell

--- myshell.c
+++ myshell.c
@@ -26,13 +26,19 @@ bool debug;
 // USer Commands
 void sigCommand(const char *pidStr, int sig);
 void cdCommand(const char *path, char *cwd);
-void execute(cmdLine *pCmdLine);
 
-// Utilities
+// Executers
+void execute(cmdLine *pCmdLine);
+int forkAndExec(char *path, char *const argv[], int in_fd, int out_fd);
+
+// Helpers 
+void runPipeline(cmdLine *left);
 bool shouldDebug(int agrc, char **argv);
 Command getCommand(const char *cmd);
 void handleRedirect(cmdLine *pCmdLine);
 void DebugMessage(char *message, bool sysError);
+bool validateNoRedirectConflict(cmdLine *left, cmdLine *right);
+void DebugChild(int pid, char *cmd);
 
 
 int main(int argc, char **argv) {
@@ -50,31 +56,78 @@ int main(int argc, char **argv) {
         if (pCmdLine == NULL) {
             continue;  // Skip to next iteration if parsing failed
         }
-        Command cmd = getCommand(pCmdLine->arguments[0]);
-        switch (cmd) {
-            case CMD_QUIT:
-                quit = true;
-                break;
-            case CMD_CD:
-                cdCommand((pCmdLine->argCount > 1 ? pCmdLine->arguments[1] : NULL), cwd);
-                break;
-            case CMD_HALT:
-                sigCommand((pCmdLine->argCount > 1 ? pCmdLine->arguments[1] : NULL), SIGSTOP);
-                break;
-            case CMD_ICE: 
-                sigCommand((pCmdLine->argCount > 1 ? pCmdLine->arguments[1] : NULL), SIGINT);
-                break;
-            case CMD_WAKEUP: 
-                sigCommand((pCmdLine->argCount > 1 ? pCmdLine->arguments[1] : NULL), SIGCONT);
-                break;
-            case CMD_EXECUTE:
-                execute(pCmdLine);
-                // Wait for child a bit
-                nanosleep(&(struct timespec){0, 500000000}, NULL);
-                break;
-        } 
-        freeCmdLines(pCmdLine);
+
+        if (pCmdLine->next) {
+            runPipeline(pCmdLine);
+        }
+        else {
+            Command cmd = getCommand(pCmdLine->arguments[0]);
+            switch (cmd) {
+                case CMD_QUIT:
+                    quit = true;
+                    break;
+                case CMD_CD:
+                    cdCommand((pCmdLine->argCount > 1 ? pCmdLine->arguments[1] : NULL), cwd);
+                    break;
+                case CMD_HALT:
+                    sigCommand((pCmdLine->argCount > 1 ? pCmdLine->arguments[1] : NULL), SIGSTOP);
+                    break;
+                case CMD_ICE:
+                    sigCommand((pCmdLine->argCount > 1 ? pCmdLine->arguments[1] : NULL), SIGINT);
+                    break;
+                case CMD_WAKEUP: 
+                    sigCommand((pCmdLine->argCount > 1 ? pCmdLine->arguments[1] : NULL), SIGCONT);
+                    break;
+                case CMD_EXECUTE:
+                    execute(pCmdLine);
+                    break;
+            } 
+            // Wait for child a bit
+            nanosleep(&(struct timespec){0, 500000000}, NULL);
+            freeCmdLines(pCmdLine);
+        }
     }
+}
+
+// Handle exactly two commands joined by a pipe
+void runPipeline(cmdLine *left) {
+    cmdLine *right = left->next;
+
+    if (!validateNoRedirectConflict(left, right))
+        return;
+
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        DebugMessage("pipe", true);
+        return;
+    }
+
+    // left: stdout -> pipefd[1]
+    int p1 = forkAndExec(
+        left->arguments[0],
+        left->arguments,
+        left->inputRedirect  ? open(left->inputRedirect,  O_RDONLY) : -1,
+        pipefd[1]
+    );
+    DebugChild(p1, left->arguments[0]);
+
+    close(pipefd[1]);
+    // right: stdin <- pipefd[0]
+    int p2 = forkAndExec(
+        right->arguments[0],
+        right->arguments,
+        pipefd[0],
+        right->outputRedirect ? open(right->outputRedirect, O_CREAT|O_WRONLY | O_TRUNC, 0666) : -1
+    );
+    DebugChild(p2, right->arguments[0]);
+
+    // parent closes both ends
+    close(pipefd[0]);
+
+    // wait for both
+    // TODO: do i need to check blocking here (&)?
+    waitpid(p1, NULL, 0);
+    waitpid(p2, NULL, 0);
 }
 
 // Iterates through user arguments, returns true if the prgoram should
@@ -104,13 +157,7 @@ void execute(cmdLine *pCmdLine) {
         }
     }
     // In parent, debug
-    if (debug) {
-        char msg[100];
-        snprintf(msg, sizeof(msg), "%s%d", "PID: ", pid);
-        DebugMessage(msg, false);
-        snprintf(msg, sizeof(msg), "%s%s", "Command: ", pCmdLine->arguments[0]);
-        DebugMessage(msg, false);
-    }
+    DebugChild(pid, pCmdLine->arguments[0]);
 
     // Set blocking
     if(pCmdLine->blocking) {
@@ -163,6 +210,9 @@ void sigCommand(const char *pidStr, int sig) {
     }
 }
 
+
+// ——— Helpers —————————————————————————————————————————————
+
 void handleRedirect(cmdLine *pCmdLine) {
     if (pCmdLine->inputRedirect != NULL) {
         close(0);
@@ -182,6 +232,34 @@ void handleRedirect(cmdLine *pCmdLine) {
     }
 }
 
+// Print an error and return false
+bool validateNoRedirectConflict(cmdLine *left, cmdLine *right) {
+    if (left->outputRedirect || right->inputRedirect) {
+        DebugMessage("can't mix pipe and I/O redirect", false);
+        return false;
+    }
+    return true;
+}
+
+// Fork a child that redirects its stdin/out, then execs.
+// If in_fd or out_fd is -1, that side isn’t redirected.
+int forkAndExec(char *path, char *const argv[],
+                         int in_fd, int out_fd)
+{
+    int pid = fork();
+    if (pid == 0) {
+        // child
+        if (in_fd  != -1) { close(STDIN_FILENO);  dup(in_fd);  }
+        if (out_fd != -1) { close(STDOUT_FILENO); dup(out_fd); }
+        // close any pipe FDs inherited
+        // (we assume parent will close its copies)
+        execvp(path, argv);
+        DebugMessage("exec failed", true);
+        exit(1);
+    }
+    return pid;
+}
+
 void DebugMessage(char *message, bool sysError){
     if(debug){
         if (sysError)
@@ -189,4 +267,12 @@ void DebugMessage(char *message, bool sysError){
         else
             fprintf(stderr, "%s\n", message);
     }
+}
+
+void DebugChild(int pid, char *cmd) {
+    char msg[100];
+    snprintf(msg, sizeof(msg), "%s%d", "PID: ", pid);
+    DebugMessage(msg, false);
+    snprintf(msg, sizeof(msg), "%s%s", "Command: ", cmd);
+    DebugMessage(msg, false);
 }
